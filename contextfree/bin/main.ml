7cf9e9e0e87801cee421a421c871dc53a8b47f4b
Docker <docker@example.com>
2024-10-17 11:15:32 +0100

    Contextfree and tests

--- contextfree/bin/main.ml
+++ contextfree/bin/main.ml
@@ -2,8 +2,8 @@ open Contextfree.Grammar
 open Contextfree.Types
 open Contextfree.Exercises
 
-let input_grammar = todo (* REPLACE [todo] WITH A GRAMMAR YOU WANT TO TEST *)
-let max_steps = 10
+let input_grammar = palindromes2 (* REPLACE [todo] WITH A GRAMMAR YOU WANT TO TEST *)
+let max_steps = 11
 
 let random_production (g : grammar) (sent : sentential_form) : int * production
     =
--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,15 +16,74 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1";
+        S --> "";
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
+(* Assuming that the empty word is not a palindrome, this grammar can be used *)
+let palindromes : grammar = 
+  {
+    symbols = [ S; A ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0A0";
+        S --> "1A1";
+        A --> "0A0";
+        A --> "1A1";
+        A --> "0";
+        A --> "1";
+        A --> "";
+      ];
+    start = S;
+  }
+(* Otherwise, including the empty word... *)
+let palindromes2 : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S0";
+        S --> "1S1";
+        S --> "0";
+        S --> "1";
+        S --> "";
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 3, medium (balanced_parentheses)*)
-let balanced_parentheses : grammar = todo
+let balanced_parentheses : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '('; ')'; '['; ']'; '{'; '}' ];
+    productions =
+      [
+        S --> "(S)";
+        S --> "[S]";
+        S --> "{S}";
+        S --> "S()";
+        S --> "S[]";
+        S --> "S{}";
+        S --> "()S";
+        S --> "[]S";
+        S --> "{}S";
+        S --> "";
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 4, hard (same_amount)
@@ -33,4 +92,19 @@ let balanced_parentheses : grammar = todo
    Hint 2: think of the language of words where the number of 0s is one greater
    than the number of 1s and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0011S";
+        S --> "1100S";
+        S --> "0S1";
+        S --> "1S0";
+        S --> "01S";
+        S --> "10S";
+        S --> ""
+      ];
+    start = S;
+  }
